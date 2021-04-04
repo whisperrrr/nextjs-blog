74010ef500bd409ef459ac78e7a6dfb1e2efa669
whisperrrr <jh731448520@163.com>
2021-04-04 23:08:19 +0800

    [Hui][feat] - add a input to get user answer and use it to do the random answer get.

--- pages/fun/yes-or-no.js
+++ pages/fun/yes-or-no.js
@@ -1,31 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./yes-or-no.module.css";
 
-export async function getStaticProps() {
-    const data = getRandomAnswer();
-
-    if (!data) {
-        return {
-            notFound: true,
-        }
+const getRandomAnswer = (userQuestion) => {
+    if (userQuestion.includes("beautiful")) {
+        return "Jin Hui"
     }
-    return {
-        props: {data},
-    }
-}
-
-const getRandomAnswer = () => {
     const randomNum = Math.random();
-    return randomNum > 0.5 ? {answer: "Yes"} : {answer: "No"};
+    return randomNum > 0.5 ? "Yes" : "No";
 }
 
-const yesOrNo = ({data}) => {
+
+const yesOrNo = () => {
+    const [userQuestion, setUserQuestion] = useState("");
+    const [randomAnswer, setRandomAnswer] = useState("Yes");
+
     return (
         <div className={styles.root}>
-            <button className={styles.button} onClick={() => location.reload()}>Keep your question in your mind and
-                click to get the answer
+            <input id="question" onChange={(event) => setUserQuestion(event.target.value)} type="text"
+                   className={styles.question} placeholder="Input Your Question Here"/>
+            <button className={styles.button} onClick={() => setRandomAnswer(getRandomAnswer(userQuestion))}>
+                click to get your yes or no answer
             </button>
-            <h1 className={styles.answer}>{data.answer}</h1>
+            <h1 className={styles.answer}>{randomAnswer}</h1>
         </div>
     )
 }
