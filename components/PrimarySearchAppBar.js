0e7b42ee87681070ac34830a1f23ec0e69284e1f
whisperrrr <jh731448520@163.com>
2021-04-13 00:32:43 +0800

    [Hui][feat] - add top bar with search bar and icon.

---
+++ components/PrimarySearchAppBar.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import {fade, makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import BookOutlinedIcon from '@material-ui/icons/BookOutlined';
+import SportsEsportsOutlinedIcon from '@material-ui/icons/SportsEsportsOutlined';
+import FormatListNumberedOutlinedIcon from '@material-ui/icons/FormatListNumberedOutlined';
+
+const useStyles = makeStyles((theme) => ({
+    grow: {
+        flexGrow: 1,
+        width: "100%"
+    },
+    navBar: {
+        backgroundColor: "#000000",
+        color: theme.palette.secondary.main
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+            overflow: "initial",
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(4),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+}));
+
+export default function PrimarySearchAppBar() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.grow}>
+            <AppBar position="static" className={classes.navBar}>
+                <Toolbar>
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        Jin Hui
+                    </Typography>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon/>
+                        </div>
+                        <InputBase
+                            placeholder="Search…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{'aria-label': 'search'}}
+                        />
+                    </div>
+                    <div className={classes.grow}/>
+                    <div className={classes.sectionDesktop}>
+                        <IconButton title="Blog" aria-label="Blog" color="secondary">
+                            <BookOutlinedIcon/>
+                        </IconButton>
+                        <IconButton title="Game" aria-label="Game" color="secondary">
+                            <SportsEsportsOutlinedIcon/>
+                        </IconButton>
+                        <IconButton title="To Do" aria-label="To Do" color="secondary">
+                            <FormatListNumberedOutlinedIcon/>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,19 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import dynamic from "next/dynamic";
 
-const name = 'Hui Jin'
+const PrimarySearchAppBar = dynamic(() => import("./PrimarySearchAppBar"), {
+    ssr: false
+})
+
 export const siteTitle = '💌'
 
-export default function Layout({ children, home }) {
+export default function Layout({children, home}) {
     return (
         <div className={styles.container}>
             <Head>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico"/>
                 <meta
                     name="description"
                     content="Learn how to build a personal website using Next.js"
@@ -22,45 +24,11 @@ export default function Layout({ children, home }) {
                         siteTitle
                     )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
                 />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="og:title" content={siteTitle}/>
+                <meta name="twitter:card" content="summary_large_image"/>
             </Head>
-            <header className={styles.header}>
-                {home ? (
-                    <>
-                        <Image
-                            priority
-                            src="/images/profile.png"
-                            className={utilStyles.borderCircle}
-                            height={144}
-                            width={144}
-                            alt={name}
-                        />
-                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
-                    </>
-                ) : (
-                    <>
-                        <Link href="/">
-                            <a>
-                                <Image
-                                    priority
-                                    src="/images/profile.png"
-                                    className={utilStyles.borderCircle}
-                                    height={108}
-                                    width={108}
-                                    alt={name}
-                                />
-                            </a>
-                        </Link>
-                        <h2 className={utilStyles.headingLg}>
-                            <Link href="/">
-                                <a className={utilStyles.colorInherit}>{name}</a>
-                            </Link>
-                        </h2>
-                    </>
-                )}
-            </header>
-            <main>{children}</main>
+            <PrimarySearchAppBar/>
+            <main className={styles.main}>{children}</main>
             {!home && (
                 <div className={styles.backToHome}>
                     <Link href="/">
